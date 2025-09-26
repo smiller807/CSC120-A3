494c871886d40d600fef3d1b488d6ea9041add14
Sarah Miller <smiller807@smith.edu>
2025-09-25 22:08:16 -0400

    Finished the code and tested multiple times

--- Conversation.java
+++ Conversation.java
@@ -1,52 +1,44 @@
 import java.util.Scanner;
 import java.util.HashMap;
 import java.util.Map;
+
 class Conversation implements Chatbot {
   // Attributes 
   private int rounds;
-  private String text;
-  private String response;
+  private String transcript;
+
+  //open scanner
+  Scanner scanner = new Scanner(System.in);
+  //create mirror list
+  Map<String, String> mirrorWords = new HashMap<>();
 
   /**
    * Constructor 
    * @param rounds how many rounds the chatbot will interact with the user given by user
-   * @param text text from the user
-   * @param response chatbot response
-   * @param words words being replaced
+   * @param transcript adds the input and output to the transcript
    */
   Conversation() {
     this.rounds = 0;
-    this.text = text;
-    this.response = response;
+    this.transcript = "";
   }
 
   //getters
   public int getRounds(int rounds){
     return this.rounds;
   }
-  public String getText(String text){
-    return this.text;
-  }
-  public String getResponse(String response){
-    return this.response;
-  }
 
   /**
    * Starts and runs the conversation with the user
    */
   public void chat() {
-    //open scanner
-    Scanner scanner = new Scanner(System.in);
-    //create mirror list
-    Map<String, String> mirrorWords = new HashMap<>();
-
     //create mirror mappings
     mirrorWords.put("I", " you");
-    mirrorWords.put("me ", "you");
-    mirrorWords.put("am ", "are");
+    mirrorWords.put("me", "you");
+    mirrorWords.put("am", "are");
     mirrorWords.put("you", "I");
     mirrorWords.put("my", "your");
     mirrorWords.put("your", "my");
+    mirrorWords.put("are", "am");
 
     //number of rounds
     System.out.println("How many rounds?: ");
@@ -54,19 +46,12 @@ class Conversation implements Chatbot {
     scanner.nextLine();
     System.out.println("Hello! What's on your mind?");
 
-    for (int i = 0; i <= rounds; i++) {
-        String text = scanner.nextLine();
-        String[] words = text.split(" ");
-        String response = "";
-
-        for (String word : words) {
-            String replacement = mirrorWords.getOrDefault(word, word);
-            if (replacement.equals("i")) {
-                replacement = "I"; // Capitalize when needed
-            }
-            response += replacement + " ";
-        }
-        System.out.println(response.trim());
+    for (int i = 0; i < rounds; i++) {
+      String text = scanner.nextLine();
+      String response = respond(text);
+      System.out.println(response);
+      transcript += "User: " + text + "\n";
+      transcript += "Chatbot: " + response + "\n";
     }
     System.out.println("Ok! bye");
     scanner.close();
@@ -76,7 +61,11 @@ class Conversation implements Chatbot {
    * Prints transcript of conversation
    */
   public void printTranscript() {
-
+    System.out.println("\n-----------");
+    System.out.println("TRANSCRIPT");
+    System.out.println("-----------");
+    System.out.println(transcript);
+    System.out.println("Ok! bye");
   }
 
   /**
@@ -84,9 +73,32 @@ class Conversation implements Chatbot {
    * @param inputString the users last line of input
    * @return mirrored or canned response to user input  
    */
-  public String respond(String inputString) {
-    String returnString = ""; 
-    return returnString; 
+  public String respond(String inpuString) {
+    String[] words = inpuString.split(" ");
+    String response = "";
+
+    //check if any mirrorWord is in the input
+    boolean containsMirror = false;
+    for (String word : words) {
+        if (mirrorWords.containsKey(word)){
+            containsMirror = true;
+            break;
+        }
+    }
+
+    if (containsMirror){
+      for (String word : words) {
+        String replacement = mirrorWords.getOrDefault(word, word);
+        if (replacement.equals("i")) {
+            replacement = "I"; // Capitalize when needed
+        }
+        response += replacement + " ";
+      }
+      return response.trim();
+    } else {
+      System.out.println("mmm ok.");
+    }
+    return inpuString;
   }
 
   public static void main(String[] arguments) {
